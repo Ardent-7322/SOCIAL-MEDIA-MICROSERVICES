521fd266ad237d8d0f7d26c5952040d57e3de759
Ardent-7322 <manishhpkumar34@gmail.com>
2025-10-12 18:29:24 +0530

    search controller created

--- search-service/src/controllers/search-controller.js
+++ search-service/src/controllers/search-controller.js
@@ -1,1 +1,29 @@
-const searchPostController = async 
+const Search = require("../models/Search");
+const logger = require("../utils/logger");
+
+const searchPostController = async (req, res) => {
+  logger.info("Search Endpoint hit...");
+  try {
+    const { query } = req.query;
+    const results = await Search.find(
+      {
+        $text: { $search: query },
+      },
+      {
+        score: { $meta: "textScore" },
+      }
+    )
+      .sort({ score: { $meta: "textScore" } })
+      .limit(10);
+
+    res.json(results);
+  } catch (e) {
+    logger.error("Error while searching post", e);
+    res.status(500).json({
+      success: false,
+      message: "Error searching post",
+    });
+  }
+};
+
+module.exports= {searchPostController}
